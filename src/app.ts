d9d770a4e382d626c761a9cf798bb271d6cd5126
David Ehiagwina <s00271237@atu.ie>
2026-09-16 09:57:47 +0100

    First Commit

---
+++ src/app.ts
@@ -0,0 +1,49 @@
+import express, {Application, Request, Response} from "express" ; 
+
+ 
+
+const PORT = process.env.PORT || 3120; 
+
+ 
+
+const app: Application = express(); 
+
+ 
+
+app.get("/ping", async (_req : Request, res: Response) => { 
+
+  res.json({ 
+
+  message: "hello from David"  
+
+  }); 
+
+});
+
+ 
+
+app.listen(PORT, () => { 
+
+  console.log("Server is running on port", PORT); 
+  }); 
+  app.get('/bananas', async (_req : Request, res: Response) => { 
+  res.json({ 
+  message: "this is bananas", 
+  }); 
+}); 
+
+app.listen(PORT, () => { 
+
+  console.log("Server is running on port", PORT); 
+  }); 
+  app.get('/crazy', async (_req : Request, res: Response) => { 
+  res.json({ 
+  message: "this is crazy", 
+  }); 
+}); 
+
+app.use((req, _res, next) => {  
+
+  console.log(`${req.method} ${req.originalUrl}`); 
+  next(); 
+});     
